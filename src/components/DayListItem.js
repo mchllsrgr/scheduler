e1fc5970b5848db40eaa0af84dc825e01cf99804
Michelle Siregar <michellesiregar@gmail.com>
2019-11-25 13:35:41 -0800

    write test for daylistitem and spot conditional

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,10 +7,20 @@ export default function DayListItem(props) {
   if (props.selected) dayClass += '--selected';
   else if (props.spots === 0) dayClass += '--full';
 
+  function formatSpots(spots) {
+    if (props.spots === 0) {
+      return 'no spots';
+    } else if (props.spots === 1) {
+      return `${spots} spot`;
+    } else if (props.spots > 1) {
+      return `${spots} spots`;
+    }
+  }
+
   return (
     <li className={dayClass} onClick={props.setDay}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
