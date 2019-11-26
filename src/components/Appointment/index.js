2367b6244f75a45142168a0ed3afc40af74d3b10
Michelle Siregar <michellesiregar@gmail.com>
2019-11-25 18:23:18 -0800

    add header

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
 
-export default function Appointment() {
+export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+    </article>
   )
 }
