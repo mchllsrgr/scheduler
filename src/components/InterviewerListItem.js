cd2d18a800678ed73099d4181e25a5abe9c6c653
Michelle Siregar <michellesiregar@gmail.com>
2019-11-25 16:25:46 -0800

    add styling to interviewer list item

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
+import classNames from 'classnames/bind';
 
 export default function InterviewerListItem({ id, name, avatar, selected, setInterviewer }) {
+  let interviewerClass = classNames("interviewers__item",
+  {"interviewers__item--selected" : selected}
+  );
+
   return (
-    <li className="interviewers__item" id={id} selected={selected} onClick={setInterviewer}>
+    <li className={interviewerClass} id={id} onClick={setInterviewer}>
       <img
         className="interviewers__item-image"
         src={avatar}
         alt={name}
       />
-      {name}
+      {selected && (<>{name}</>)}
     </li>
   )
 }
