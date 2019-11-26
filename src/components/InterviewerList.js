a693caf187ec7e7b524b4b4a37cc482e5a74b428
Michelle Siregar <michellesiregar@gmail.com>
2019-11-25 17:08:21 -0800

    import scss for interviewer list

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import InterviewerListItem from "components/InterviewerListItem";
+import  "components/InterviewerList.scss";
 import "components/InterviewerListItem.scss";
 
 
