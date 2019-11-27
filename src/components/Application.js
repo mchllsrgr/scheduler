bb386baabde07838276d2ee7823030a6d940f502
Michelle Siregar <michellesiregar@gmail.com>
2019-11-26 19:05:36 -0800

    refactor application states in object

--- src/components/Application.js
+++ src/components/Application.js
@@ -56,14 +56,19 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
 
   useEffect(() => {
     axios
     .get('/api/days')
     .then((response) => {
-      setDays(response.data);
+      state.setDays(response.data);
     })
     .catch((error) => console.log(error))
   }, [])
@@ -79,9 +84,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-          days={days}
-          day={day}
-          setDay={setDay}
+          days={state.days}
+          day={state.day}
+          setDay={state.setDay}
            />
         </nav>
         <img
