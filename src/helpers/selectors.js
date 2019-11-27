8174f2953e4fad0de9104d6a1505eb2acb8d333e
Michelle Siregar <michellesiregar@gmail.com>
2019-11-26 18:19:41 -0800

    add appointmentsforday helper function to format api results

---
+++ src/helpers/selectors.js
@@ -0,0 +1,11 @@
+export function getAppointmentsForDay(state, day) {
+  const result = [];
+  for (let i = 0; i < state.days.length; i++) {
+    if (state.days[i].name === day) {
+      state.days[i].appointments.forEach(function(e) {
+        result.push(e = state.appointments[e])
+      })
+    }
+  }
+  return result;
+}
