752628229a13ead73f7fc8cc3a9b5dca04e24a09
Michelle Siregar <michellesiregar@gmail.com>
2019-11-27 16:44:17 -0800

    complete transition and back modes

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  function transition(initial) {
-    setMode(initial);
+  function transition(newMode) {
+    setHistory([...history, newMode]);
+    setMode(newMode);
   }
 
-  return { mode, transition };
+  function back() {
+    let newHist = [...history].slice(0, history.length-1)
+    setHistory(newHist);
+    setMode(newHist[newHist.length - 1]);
+  }
+
+  return { mode, transition, back };
 }
