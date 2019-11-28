937fabd46a5fe9a494233d26a0ebf18bd5986d0f
Michelle Siregar <michellesiregar@gmail.com>
2019-11-27 17:30:38 -0800

    add conditional for transition replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,13 +4,20 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
-    setHistory([...history, newMode]);
+  function transition(newMode, replace) {
+    let newHist = [...history, newMode]
+    setHistory(newHist);
     setMode(newMode);
+
+    if (replace === true) {
+      newHist.splice(newHist.length-2, 2, newMode);
+      setHistory(newHist)
+      setMode(newHist[newHist.length - 1])
+    }
   }
 
   function back() {
-    let newHist = [...history].slice(0, history.length-1)
+    let newHist = [...history].slice(0, history.length-1);
     if (newHist.length >= 1) {
       setHistory(newHist);
       setMode(newHist[newHist.length - 1]);
